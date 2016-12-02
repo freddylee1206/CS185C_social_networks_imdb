e02aad22f626625c359e1d7cec18dd478159a6ae
akalenda <akalenda@users.noreply.github.com>
2016-12-01 16:20:05 -0800

    added ability to give last layer complete data

--- src/PullIMDbData.py
+++ src/PullIMDbData.py
@@ -1,32 +1,30 @@
-import networkx as nx
 import pandas
 from imdb import IMDb
-from networkx_viewer import Viewer
 
 
 imdbapi = IMDb()
-seed_movie = '2404435'
+seed_movie = 'm2404435'
 
 nodes = pandas.DataFrame(columns=['id', 'type', 'name', 'edges'])
 
 
-def add_movie(movie_id):
+def add_movie(movie_id, final=False):
     global nodes
-    movie = imdbapi.get_movie(movie_id)
-    edges = set([person.getID() for person in (movie['cast'] + movie['director'])])
+    movie = imdbapi.get_movie(movie_id[1:])
+    edges = set(['p' + person.getID() for person in (movie['cast'] + movie['director'])]) if not final else set()
     newRow = pandas.DataFrame({'id': [movie_id], 'type': ['movie'], 'name': [movie.get('title')], 'edges': [edges]})
     nodes = pandas.concat([nodes, newRow])
 
 
-def add_person(person_id):
+def add_person(person_id, final=False):
     global nodes
-    filmography = imdbapi.get_person_filmography(person_id)
+    filmography = imdbapi.get_person_filmography(person_id[1:])
     directed_movies = filmography['data']['director'] if 'director' in filmography['data'] else []
-    directed_movies = set([movie.getID() for movie in directed_movies])
+    directed_movies = set(['m' + movie.getID() for movie in directed_movies])
     acted_movies = filmography['data']['actor'] if 'actor' in filmography['data'] else []
-    acted_movies = set([movie.getID() for movie in acted_movies])
+    acted_movies = set(['m' + movie.getID() for movie in acted_movies])
     name = filmography['data']['name']
-    edges = directed_movies.union(acted_movies)
+    edges = directed_movies.union(acted_movies) if final else set()
 
     if directed_movies and acted_movies:
         node_type = 'director/actor'
@@ -41,10 +39,20 @@ def add_person(person_id):
     nodes = pandas.concat([nodes, newRow])
 
 
+def expand_graph(final=False):
+    all_edges = set([destination_node for edge_list in nodes['edges'] for destination_node in edge_list])
+    dangling_edges = all_edges.difference(nodes['id'])
+    for node_id in dangling_edges:
+        if node_id[0] is 'p':
+            add_person(node_id, final=final)
+        elif node_id[1] is 'm':
+            add_movie(node_id, final=final)
+        else:
+            raise Exception("mistagged node_id {}".format(node_id))
+
+
 add_movie(seed_movie)
-all_edges = set([destination_node for edge_list in nodes['edges'] for destination_node in edge_list])
-pending_people = all_edges.difference(nodes['id'])
-for node_id in pending_people:
-    add_person(node_id)
+expand_graph()
+expand_graph(final=True)
 nodes.to_csv("nodes.csv")
 print "fin."
