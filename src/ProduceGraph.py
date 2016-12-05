98b18174fc66881d3bf0ba576b97d035f243d70f
akalenda <akalenda@users.noreply.github.com>
2016-12-05 00:43:49 -0800

    Added degree column and various seaborn plots

--- src/ProduceGraph.py
+++ src/ProduceGraph.py
@@ -1,4 +1,6 @@
 import pandas
+import seaborn
+from matplotlib import pyplot
 from networkx import Graph, shortest_path
 from networkx.algorithms.bipartite import projected_graph
 from networkx.algorithms.centrality import betweenness_centrality
@@ -13,7 +15,7 @@ nodes = pandas.read_csv('nodes.csv', index_col='id')
 nodes['edges'] = nodes['edges'].apply(lambda string: set(string[5:-2].split(', ')))
 people = []
 
-for node_id, (edges, name, node_type, clusteringCoefficient, betweennessCentrality, distance) in nodes.iterrows():
+for node_id, (edges, name, node_type, clusteringCoefficient, betweennessCentrality, distance, degree) in nodes.iterrows():
     G.add_node(node_id, attr_dict={
         'id_': node_id,
         'name': name,
@@ -21,7 +23,8 @@ for node_id, (edges, name, node_type, clusteringCoefficient, betweennessCentrali
         'edges': edges,
         'clusteringCoefficient': clusteringCoefficient,
         'betweennessCentrality': betweennessCentrality,
-        'distanceToFurthestNode': distance
+        'distanceToFurthestNode': distance,
+        'degree': degree
     })
     if node_id[0] == 'p':
         people.append(node_id)
@@ -42,12 +45,25 @@ for source, targets_to_paths in shortest_path(pG).iteritems():
 nodes['distanceToFurthestNode'] = pandas.Series(distances_to_furthest_nodes, index=nodes.index)
 nodes['clusteringCoefficient'] = pandas.Series(clustering(pG), index=nodes.index)
 nodes['betweennessCentrality'] = pandas.Series(betweenness_centrality(pG), index=nodes.index)
+nodes['degree'] = pandas.Series(pG.degree(), index=nodes.index)
 
 print "vertices: {}".format(pG.number_of_nodes())
 print "edges: {}".format(pG.number_of_edges())
 print nodes.nlargest(5, 'clusteringCoefficient')[['clusteringCoefficient', 'name']]
 print nodes.nlargest(5, 'betweennessCentrality')[['betweennessCentrality', 'name']]
 print nodes.nlargest(5, 'distanceToFurthestNode')[['distanceToFurthestNode', 'name']]
+print nodes.nlargest(5, 'degree')[['degree', 'name']]
+
 nodes.to_csv('nodes.csv', encoding='utf-8')
+
 app = Viewer(pG)
 app.mainloop()
+
+p_nodes = nodes.loc[~nodes['type'].isin(['error', 'movie'])]
+seaborn.FacetGrid(data=p_nodes, hue='type', palette=['red', 'blue', 'green']) \
+    .map(pyplot.scatter, 'betweennessCentrality', 'clusteringCoefficient') \
+    .add_legend()
+pyplot.show()
+
+seaborn.swarmplot(x="type", y="degree", data=p_nodes)
+pyplot.show()
